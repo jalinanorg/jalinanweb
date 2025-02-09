7732eb948148390007a2934ae6703e2699a5e5b1
rusdyahmad <rusdyahmad@users.noreply.github.com>
2025-02-09 17:53:06 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    allowedHosts: [
+      "localhost",
+      "*.ondigitalocean.app",
+      "jalinan.org",
+      "*.jalinan.org",
+    ],
+  },
+});
